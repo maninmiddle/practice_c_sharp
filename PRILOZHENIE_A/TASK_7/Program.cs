5a311af542c5829c68866083a889e47abfcadc91
Roman Mayorau <baeyer@Alexanders-MacBook-Air.local>
2026-03-16 12:22:23 +0300

    feat: add task7 to prilozhenie A

---
+++ PRILOZHENIE_A/TASK_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("x1 = ");
+        double x1 = double.Parse(Console.ReadLine());
+        Console.Write("y1 = ");
+        double y1 = double.Parse(Console.ReadLine());
+        Console.Write("x2 = ");
+        double x2 = double.Parse(Console.ReadLine());
+        Console.Write("y2 = ");
+        double y2 = double.Parse(Console.ReadLine());
+
+        double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+        Console.WriteLine($"Расстояние = {d:F4}");
+ 
+    }
+}
