174b42dfead7b7064140e98aadae7c7da3f56366
Roman Mayorau <baeyer@Alexanders-MacBook-Air.local>
2026-03-16 12:25:41 +0300

    feat: add task8 to prilozhenie A

---
+++ PRILOZHENIE_A/TASK_8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        double x = 4.3;
+
+        double y = (1 + Math.Sqrt(3 - Math.Pow(x, 2)) / Math.Atan(Math.Pow(x, 2))) - Math.Exp(Math.Sin(Math.Sqrt(x)));
+
+        Console.WriteLine($"x = {x}");
+        Console.WriteLine($"y = {y:F4}");
+    }
+}
