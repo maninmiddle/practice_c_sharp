812b841fbae2a490e3c169363263cc678d93c823
Roman Mayorau <baeyer@Alexanders-MacBook-Air.local>
2026-03-16 12:20:27 +0300

    feat: add task6 to prilozhenie A

---
+++ PRILOZHENIE_A/TASK_6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        double x = 3.5;
+
+        double y = Math.Pow(Math.Cos(x), 2) - (Math.Sqrt(Math.Pow(x, 3)) + 1) / Math.Sin(x) + Math.Exp(Math.Log(2 * x));
+
+        Console.WriteLine($"x = {x}");
+        Console.WriteLine($"y = {y:F4}");
+
+    }
+}
