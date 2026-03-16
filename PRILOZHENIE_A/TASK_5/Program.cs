1b383199a924150714cade76e1ef06601d57c10c
Roman Mayorau <baeyer@Alexanders-MacBook-Air.local>
2026-03-16 12:16:45 +0300

    feat: add task5 to prilozhenie A

---
+++ PRILOZHENIE_A/TASK_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите четырехзначное число: ");
+        string inputNumberStr = Console.ReadLine();
+        int number = int.Parse(inputNumberStr);
+        
+        int sumOfDigits = 0;
+        foreach (char digit in inputNumberStr)
+        {
+            sumOfDigits += int.Parse(digit.ToString());
+        }
+        Console.WriteLine($"Сумма цифр: {sumOfDigits}\n");
+    }
+}
