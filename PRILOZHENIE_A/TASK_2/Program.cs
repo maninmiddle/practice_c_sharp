f1468fad2fe559285dc3f447223a9f1d5859f857
Roman Mayorau <baeyer@Alexanders-MacBook-Air.local>
2026-03-16 12:05:55 +0300

    feat: add task2 to prilozhenie A

---
+++ PRILOZHENIE_A/TASK_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите двузначное число: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        int first = number / 10;
+        int last = number % 10;
+        
+        Console.WriteLine($"Первая цифра: {first}");
+        Console.WriteLine($"Последняя цифра: {last}\n");
+    }
+}
