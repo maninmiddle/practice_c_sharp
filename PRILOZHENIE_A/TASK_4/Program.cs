9f7c9a09557635b08ff202991d9087600dd5a1a9
Roman Mayorau <baeyer@Alexanders-MacBook-Air.local>
2026-03-16 12:15:29 +0300

    feat: add task4 to prilozhenie A

---
+++ PRILOZHENIE_A/TASK_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите первое целое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе целое число: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Сумма: {num1 + num2}\n");
+    }
+}
