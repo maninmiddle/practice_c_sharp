dbeea4fe0fdea6f81a87b37442b7d1daa3a279cd
Roman Mayorau <baeyer@Alexanders-MacBook-Air.local>
2026-03-16 12:11:32 +0300

    feat: add task3 to prilozhenie A

---
+++ PRILOZHENIE_A/TASK_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите значение a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        double z1 = Math.Sin(Math.PI / 2 + 3 * a) / (1 - Math.Sin(3 * a - Math.PI));
+        double z2 = 1.0 / Math.Tan((5.0 / 4.0) * Math.PI + (3.0 / 2.0) * a);
+
+        Console.WriteLine($"z1 = {z1:F4}");
+        Console.WriteLine($"z2 = {z2:F4}");
+    }
+}
