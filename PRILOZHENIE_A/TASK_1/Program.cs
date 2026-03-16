f1e4f5685c501e812276d42940b7b3c0026e4132
Roman Mayorau <baeyer@Alexanders-MacBook-Air.local>
2026-03-16 12:03:14 +0300

    feat: add task1 to prilozhenie A

---
+++ PRILOZHENIE_A/TASK_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main() 
+    {
+        Console.Write("Введите Радиус (см): ");
+        if (double.TryParse(Console.ReadLine(), out double r))
+        {
+            double circleArea = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine($"Площадь круга: {circleArea:F2} кв.см.");
+        }
+    }
+}
